5acfdef6b935470248ed4b86fae3a3474181d58e
Bohdan <bogdan.sharipov@yandex.ru>
2015-06-10 04:16:09 +0300

    Almost userPage

---
+++ src/main/java/com/bionic/edu/bean/AvailibleIncomeBean.java
@@ -0,0 +1,48 @@
+package com.bionic.edu.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.context.annotation.Scope;
+
+import com.bionic.edu.entity.Income;
+import com.bionic.edu.service.IncomeService;
+
+@Named
+@Scope("session")
+public class AvailibleIncomeBean {
+	private List<Income> incomes = null;
+	public Income income = null;
+	private List<Income> bucket = new ArrayList<Income>();
+	
+	@Inject
+	private IncomeService incomeService;	
+	
+	public AvailibleIncomeBean(){
+	}
+	
+	public void RefreshList(){
+		incomes = incomeService.getAvailibleForSellFish();
+
+	}
+
+	public Income getIncome() {
+		return income;
+	}
+
+	public List<Income> getIncomes() {
+		return incomes;
+	}
+	
+	public String addToBucket(String id){
+		int n = Integer.valueOf(id);
+		bucket.add(incomeService.findById(n));
+		return "bucket";
+	}
+	
+	
+		
+}
