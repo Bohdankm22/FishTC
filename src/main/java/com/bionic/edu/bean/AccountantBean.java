855973d12161361bc201764d46ea84dd979ad901
Bohdan <bogdan.sharipov@yandex.ru>
2015-06-23 17:03:06 +0300

    Accountant
    form with prepayment type editing

--- src/main/java/com/bionic/edu/bean/AccountantBean.java
+++ src/main/java/com/bionic/edu/bean/AccountantBean.java
@@ -2,9 +2,12 @@ package com.bionic.edu.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.RowEditEvent;
 import org.springframework.context.annotation.Scope;
 
 import com.bionic.edu.entity.Income;
@@ -51,16 +54,34 @@ public class AccountantBean {
 	public void refreshIncomes(){
 		incomes = incomeService.getAvailibleForSellFish();
 	}
-	
-	public String setPreType(String id){
-		int n = Integer.valueOf(id);
-		return "";
-	}
 
 	public List<Income> getIncomes() {
 		return incomes;
 	}
+
 	
+	public void onRowEdit(RowEditEvent event) {
+		Income i = (Income) event.getObject();
+		if(incomeService.findById(i.getIncome_id()) != null){
+			incomeService.update(i);
+		}
+        FacesMessage msg = new FacesMessage("Income edited", i.getIncome_Itemname());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 	
+	public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Income) event.getObject()).getIncome_Itemname());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+	
+	public String type(String tp){
+		int n = Integer.valueOf(tp);
+		switch(n){
+		case 1: return "Half prepayment(50%)";
+		case 2: return "Partial prepayment (75%)";
+		case 3: return "Full prepayment (100%)";
+		default: return "It's mistake";
+		}
+	}
 	
 }
