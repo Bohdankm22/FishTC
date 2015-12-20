3cb6a7ce740fd2792d5421cb41870c74f19118e1
Bohdan <bogdan.sharipov@yandex.ru>
2015-12-20 23:51:17 +0200

    Student enter page and Student enter bean created. login and pass were check in Oracle DB within logging in

---
+++ src/main/java/com/bionic/edu/bean/StudentEnterBean.java
@@ -0,0 +1,61 @@
+package com.bionic.edu.bean;
+
+import com.bionic.edu.entity.Students;
+import com.bionic.edu.service.StudentsService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.inject.Named;
+
+@Named
+@Scope("session")
+public class StudentEnterBean {
+    private String login = null;
+    private String pass = null;
+    private Students studentsPers = null;
+
+    private static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    private static StudentsService studentsService = context.getBean(StudentsService.class);
+//    @Inject
+//    private StudentsService studentsService;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String findStudent() {
+        studentsPers = studentsService.findByLoginPass(login, pass);
+        if(studentsPers != null) {
+            return "studentPage";
+        }
+        else {
+            return "error403";
+        }
+    }
+
+    public Students getStudentsPers() {
+        return studentsPers;
+    }
+
+    public void setStudentsPers(Students studentsPers) {
+        this.studentsPers = studentsPers;
+    }
+
+    public void studentNull(){
+        studentsPers = null;
+    }
+
+}
