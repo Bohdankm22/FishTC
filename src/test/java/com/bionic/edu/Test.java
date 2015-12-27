943570ca3cab853ba68d1bd2e8482b6db9304fc8
Bohdan <bogdan.sharipov@yandex.ru>
2015-12-27 12:21:12 +0200

    Adding method to add data for Student table

--- src/test/java/com/bionic/edu/Test.java
+++ src/test/java/com/bionic/edu/Test.java
@@ -1,5 +1,52 @@
 package com.bionic.edu;
 
+import com.bionic.edu.entity.Groups;
+import com.bionic.edu.entity.Students;
+import com.bionic.edu.service.GroupService;
+import com.bionic.edu.service.LessonService;
+import com.bionic.edu.service.StudentsService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Test {
 
+    private static ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    private static StudentsService studentsService = context.getBean(StudentsService.class);
+    private static GroupService groupService = context.getBean(GroupService.class);
+    private static LessonService lessonService = context.getBean(LessonService.class);
+    private static final List<String> studNames = Arrays.asList("Bohdan_Ivanov", "Ivan_Bozhenko", "Petro_Sagaydak", "Vasily_Rybka", "Evgeny_Ozerny");
+
+    public static void main(String[] args) {
+        Groups groups = new Groups();
+//        groups.se
+    }
+
+    public Students getStudByLogin(String login, String pass) {
+        return studentsService.findByLoginPass(login, pass);
+    }
+
+    public void saveStudent(String name, String login, String pass) {
+        Students student = new Students();
+        student.setStudents_isDeleted(false);
+        student.setStudents_Login(login);
+        student.setStudents_Name(name);
+        student.setStudents_password(pass);
+        studentsService.save(student);
+    }
+
+    public static void saveStudents(List<String> studentsNames) {
+        for (String studName : studentsNames) {
+            Test test1 = new Test();
+            test1.saveStudent(studName, studName, "1111");
+            System.out.println(studentsService.getAll());
+        }
+    }
+
+    public static void saveGroups(List<Groups> groups) {
+
+    }
 }
